32814a98ec0bc963bf046b4f7aca4bbb6a4cbfd0
Chandrayana Putra <shc6669@gmail.com>
2022-03-19 23:44:54 +0800

    fix index

--- resources/views/candidate/index.blade.php
+++ resources/views/candidate/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('page-title', 'Manage Orders')
-@section('page-heading', 'Car Repair Management')
+@section('page-title', 'Manage Data')
+@section('page-heading', 'Candidate Management')
 
 @section('breadcrumbs')
     <li class="breadcrumb-item active">
-        Car Repair Management
+        Candidate Management
     </li>
 @stop
 
@@ -16,13 +16,13 @@
 <div class="card">
     <div class="card-body">
 
-        @if(auth()->user()->hasRole('Admin'))
+        @if(auth()->user()->hasRole('Admin_HRD'))
         <div class="row mb-3 pb-3 border-bottom-light">
 			<div class="col-lg-12">
 				<div class="float-right">
-					<a href="{{ route('orders.create') }}" class="btn btn-primary btn-rounded float-right">
+					<a href="{{ route('candidate-management.create') }}" class="btn btn-primary btn-rounded float-right">
 						<i class="fas fa-plus mr-2"></i>
-						@lang('Add Order')
+						@lang('Add Data')
 					</a>
 				</div>
 			</div>
@@ -32,14 +32,12 @@
         <div class="container">
             <div class="row">
                 <div class="table-responsive">
-                    <table class="table table-borderless table-hover" id="orders-table-wrapper">
+                    <table class="table table-borderless table-hover" id="candidate-table-wrapper">
                         <thead class="thead-dark">
                             <tr>
                                 <th></th>
-                                <th class="min-width-80">@lang('Owners Name')</th>
-                                <th class="min-width-80">@lang('Cars Name')</th>
-                                <th class="min-width-80">@lang('Licence Plate')</th>
-                                <th class="min-width-80">@lang('Status')</th>
+                                <th class="min-width-80">@lang('Applicant Name')</th>
+                                <th class="min-width-80">@lang('Applicant Email')</th>
                                 <th class="min-width-90">@lang('Action')</th>
                             </tr>
                         </thead>
@@ -57,23 +55,21 @@
 
 @section('scripts')
     <script type="text/javascript">
-        DataTableElement = $('#orders-table-wrapper');
+        DataTableElement = $('#candidate-table-wrapper');
         TableColumns = [
             {data: "DT_RowIndex", orderable:false, filter: false, searchable: false},
-            {data: "car_owner", name:"car_owner", orderable:false, filter: false},
-            {data: "car_name", name:"car_name", orderable:false, filter: false},
-            {data: "licence_plate", name:"licence_plate", orderable:false, filter: false},
-            {data: "car_status", name:"car_status", orderable:false, filter: false},
+            {data: "name", name:"name", orderable:false, filter: false},
+            {data: "email", name:"email", orderable:false, filter: false},
             {data: "action", name: "action",orderable: false, searchable: false}
         ];
 
         var Datatable = {
             "init" : function(){
-                dtcars = {
+                dtcandidate = {
                     /*destroy: true,*/
                     processing: true,
                     serverSide: true,
-                    ajax: '{{ route("get.orders") }}',
+                    ajax: '{{ route("get.candidate") }}',
                     columns: TableColumns,
                     columnDefs: [{
                         targets: 0,
@@ -81,7 +77,7 @@
                         className: 'dt-body-center'
                     }],
                     responsive:!0, lengthMenu:[[5, 10, 15, 20, -1], [5, 10, 15, 20, "All"]],pageLength:15,
-                }; DataTableElement.DataTable(dtcars);
+                }; DataTableElement.DataTable(dtcandidate);
             }
         };
 
